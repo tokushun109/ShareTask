2387e325e9e115d03a3dfc636b25f66b914116a7
Shunta Tokunaga <tokushun109@gmail.com>
2019-10-16 02:00:06 +0000

    heroku2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,14 @@
 class ToppagesController < ApplicationController
+  before_action :toppage_redirect, only: [:index]
+  
   def index
   end
 end
+
+private
+
+    def toppage_redirect
+        if logged_in?
+          redirect_to groups_url
+        end
+    end
