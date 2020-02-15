ce8a9f8a05aabd2d2f94d16d4625cc19fe258de5
shunta.tokunaga <tokushun109@gmail.com>
2020-02-15 22:34:00 +0900

    ユーザーが招待を承認したらメンバーに通知メールが送られる機能を実装3

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -26,7 +26,9 @@ class RelationshipsController < ApplicationController
     @relationship = group.relationships.find_by(user_id: current_user.id)
     @relationship.status = 'accept'
     @relationship.save
-    group.users.each(&send_acception_email(:current_user, :current_group))
+    group.users.each do |user|
+      user.send_acception_email(current_user, current_group)
+    end
     flash[:success] = 'グループに参加しました'
     redirect_to groups_url
   end
