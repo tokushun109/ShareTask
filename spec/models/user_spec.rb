b9ae66fa2417ce4831ba8cf229a69ee2ac0c30ef
shunta.tokunaga <tokushun109@gmail.com>
2020-04-03 23:21:57 +0900

    user_spec.rbの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,12 +69,20 @@ RSpec.describe User, type: :model do
         expect(duplicate_user).to be_invalid
       end
     end
-    context '有効ではない値を入れた時' do
-      it 'invalidになること' do
-        valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                             first.last@foo.jp alice+bob@baz.cn]
+    context '有効な値を入れた時' do
+      it 'validになること' do
+        valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
         valid_addresses.each do |valid_address|
           user.email = valid_address
+          expect(user).to be_valid
+        end
+      end
+    end
+    context '有効ではない値を入れた時' do
+      it 'invalidになること' do
+        invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+        invalid_addresses.each do |invalid_address|
+          user.email = invalid_address
           expect(user).to be_invalid
         end
       end
